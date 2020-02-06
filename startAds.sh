1ee898eb7ac6e3c2b4e8b96c582a6d5a195af8b8
mattgambill <gambill.46@osu.edu>
2020-02-06 11:41:05 -0500

    first commit

---
+++ startAds.sh
@@ -0,0 +1,8 @@
+#!/bin/csh -f
+# This script starts Keysight ADS
+# Date created: 02/06/2020
+# Author: Matt Gambill, Ohio State University
+
+echo Starting ADS...
+source /opt/local/ADS2020_update1.0/Startup.EE
+ads &
